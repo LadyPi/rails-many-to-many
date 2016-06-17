b34bffb2a627b179c75bb7ba5b563c0036fc909d
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-06-16 17:59:35 -0700

    initial commit2

---
+++ RelationshipsBetweenModels/movies/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Actor.destroy_all
+Actor.create([{first_name: "Johnny", last_name: "Depp"}, {first_name: "Angelina", last_name: "Jolie"}, {first_name: "Brad", last_name: "Pitt"}])
+Movie.create([{title: "Edward Scissorhands", description: "Wierd finger knives and haircuts.", year: "1990"}, {title: "Mr. and Mrs. Smith", description: "Crazy Couple.", year: "2005"}, {title: "Changeling", description: "Kidnapped Child.", year: "2008"}])
+
+
